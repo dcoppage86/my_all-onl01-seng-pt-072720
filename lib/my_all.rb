e3ff6aaadbf73ca81be52aa9f58fd8067d5ea896
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 21:36:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -16,11 +16,11 @@ def my_all?(collection)
 end
 
 def my_all?(collection)
-  i = 0 
+  i = 0
   block_return_values = []
-  while i < collection.length 
+  while i < collection.length
     block_return_values << yield(collection[i])
-    i = i + 1 
+    i = i + 1
   end
 end
   
